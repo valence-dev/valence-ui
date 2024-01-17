2a8802978b418998729d8b2ad7a84cd533856b80
Isaac Shea <isaac_shea@icloud.com>
2024-01-17 15:48:17 +1000

    Update DropdownContainer.tsx

--- packages/core/src/components/inputs/DropdownContainer/DropdownContainer.tsx
+++ packages/core/src/components/inputs/DropdownContainer/DropdownContainer.tsx
@@ -14,6 +14,7 @@ import { CSSProperties } from "styled-components";
 
 
 export type DropdownContainerEventProps = {
+  /** A callback called when an item it selected. */
   onSelect?: (option: Option) => void;
 }
 
@@ -21,17 +22,26 @@ export type DropdownContainerProps =
   InputContainerProps
   & DropdownContainerEventProps
   & {
+    /** A list of options to display. */
     options: Option[];
+    /** The placeholder text to display when no option is selected. */
     placeholder?: string;
 
+    /** The currently selected option. */
     selected?: number | null;
+    /** A callback to set the selected option. */
     setSelected?: (option: number | null) => void;
+    /** The currently highlighted option. */
     highlighted?: number | null;
+    /** A callback to set the highlighted option. */
     setHighlighted?: (option: number | null) => void;
 
+    /** An icon to display in the input container. */
     icon?: ReactNode;
+    /** A secondary icon to display in the input container. */
     secondaryIcon?: ReactNode;
 
+    /** Optional styles to pass to the dropdown. */
     dropdownStyle?: CSSProperties;
   }
 
