c8954e9a10a5b18041ef989d22734adfdf5c3387
Isaac Shea <isaac_shea@icloud.com>
2024-06-11 19:23:08 +1000

    Fixing the fix for my FAB

--- packages/app/src/components/buttons/FAB/FAB.tsx
+++ packages/app/src/components/buttons/FAB/FAB.tsx
@@ -45,13 +45,13 @@ export const FAB = forwardRef(function FAB(
     position: "fixed",
     zIndex: zIndex,
     bottom: vPosition !== "bottom" ? undefined :
-      `calc(env(safe-area-inset-bottom) + ${breakpoint.isMobile ? offset + 60 : offset}`,
+      `calc(env(safe-area-inset-bottom) + ${breakpoint.isMobile ? offset + 60 : offset})`,
     top: vPosition !== "top" ? undefined :
-      `calc(env(safe-area-inset-top) + ${offset}`,
+      `calc(env(safe-area-inset-top) + ${offset})`,
     left: hPosition !== "left" ? undefined :
-      `calc(env(safe-area-inset-left) + ${offset}`,
+      `calc(env(safe-area-inset-left) + ${offset})`,
     right: hPosition !== "right" ? undefined :
-      `calc(env(safe-area-inset-right) + ${offset}`,
+      `calc(env(safe-area-inset-right) + ${offset})`,
 
     ...style
   }
